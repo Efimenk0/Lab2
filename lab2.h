a982310765c8087dc9a342ff15f367f09aea9507
Efimenk0 <72745120+Efimenk0@users.noreply.github.com>
2021-01-28 23:36:50 +0300

    Add files via upload

---
+++ lab2.h
@@ -0,0 +1,14 @@
+#include "DeltoidClass/Deltoid.h"
+#include <iostream>
+
+using namespace Del;
+
+namespace Lab2 {
+
+	void getDouble(double&);
+
+	int dialog(Deltoid&);
+
+	void showOptions();
+
+}
